9c62409c664375a544c87616ed53729958eca005
Rene Presedo <presedo93@gmail.com>
2022-06-11 12:29:25 +0200

    ✨ Added Live stream selector

--- src/components/parameters/Params.tsx
+++ src/components/parameters/Params.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { Modal, Form } from "react-bootstrap";
-import { BiBroadcast } from "react-icons/bi";
 import { useTheme } from "../../stores/settings";
 
+import TitleParams from "./TitleParams";
 import FetchParams from "./FetchParams";
 import LiveParams from "./LiveParams";
 
@@ -16,28 +16,19 @@ const Params = (props: ParamsProps) => {
     const bgcolor = dark ? "bg-zinc-700" : "bg-gray-400";
     const icolor = dark ? "text-neutral-300" : "text-neutral-700";
 
-    const [mode, setMode] = useState(false);
+    const [live, setLive] = useState(false);
 
     const paramOptions = () => {
-        return (!mode) ? <FetchParams /> : <LiveParams />
+        return (!live) ? <FetchParams /> : <LiveParams />
     }
 
     return (
         <Modal show={props.show} onHide={props.toggle}>
             <Modal.Header className={bgcolor + " border-0 flex justify-center"}>
-                <Modal.Title className={icolor + " ml-12"}>Parameters</Modal.Title>
-                <BiBroadcast className={icolor + " text-xl"} />
+                <TitleParams live={live} setLive={setLive}/>
             </Modal.Header>
             <Modal.Body className={bgcolor + " flex justify-center"}>
                 <Form className={icolor}>
-                    <Form.Check
-                        className="self-center"
-                        type="switch"
-                        id="mode"
-                        label="Enable live stream"
-                        onChange={() => setMode(!mode)}
-                    />
-                    <br />
                     {paramOptions()}
                 </Form>
             </Modal.Body>
---
+++ src/components/parameters/TitleParams.tsx
@@ -0,0 +1,37 @@
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import { useTheme } from "../../stores/settings";
+import Tooltip from 'react-bootstrap/Tooltip';
+import { BiBroadcast } from "react-icons/bi";
+import Button from 'react-bootstrap/Button';
+import { Modal } from "react-bootstrap";
+
+interface TitleProps {
+    live: boolean
+    setLive: (live: boolean) => void;
+}
+
+const TitleParams = (props: TitleProps) => {
+    const { dark } = useTheme();
+    const icolor = dark ? "text-neutral-300" : "text-neutral-700";
+    const bcolor = (props.live) ? "text-blue-300" : icolor;
+
+    return (
+        <>
+            <Modal.Title className={icolor + " ml-12"}>Parameters</Modal.Title>
+            <OverlayTrigger
+                placement={"bottom"}
+                overlay={
+                    <Tooltip>
+                        Enable Live Streaming
+                    </Tooltip>
+                }
+            >
+                <Button className="bg-transparent border-0 shadow-none" onClick={() => props.setLive(!props.live)}>
+                    <BiBroadcast className={bcolor + " text-2xl hover:invert"} />
+                </Button>
+            </OverlayTrigger>
+        </>
+    )
+};
+
+export default TitleParams;
