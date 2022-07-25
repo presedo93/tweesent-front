70d51d1b73024394d2a27ba7eab57fb604083b4f
Rene Presedo <presedo93@gmail.com>
2022-07-25 21:08:46 +0200

    🚧 Adding transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,32 @@
-import { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { useState } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 // Components
-import Bar from "./components/Bar";
-import Search from "./components/Search";
-import Sentiment from "./components/Sentiment";
-import Metrics from "./components/Metrics";
+import Bar from './components/Bar'
+import Search from './components/Search'
+import Sentiment from './components/Sentiment'
+import Metrics from './components/Metrics'
 
 // Stores
-import { useTheme } from "./stores/themes";
+import { useTheme } from './stores/themes'
 
 // Interfaces
-import { TweetSentiments, TweetBody } from "./types/tweets";
+import { TweetSentiments, TweetBody } from './types/tweets'
 
 export default function App() {
-    const { dark } = useTheme();
-    const bgcolor = dark ? "bg-zinc-600" : "bg-gray-300";
+    const { dark } = useTheme()
+    const bgcolor = dark ? 'bg-zinc-600' : 'bg-gray-300'
 
     const [tweets, setTweets] = useState<TweetSentiments>({
-        positives: [{
-            id: "1547715207610265603",
-            text: "@ConchoLopez10 @AlonsoChm26 @Vellista4 @DRAbdiel_ @sudanalytics_ Salieron todos de acá:\n- Julián Álvarez (Man. City)\n- Lautaro (Inter)\n- Joaquín Correa (Inter)\n- Lisandro Martínez (Man. United)\n- Romero (Tottenham)\n- Lo Celso (Tottenham)\n- Di María (Juventus)\n- Paredes  (PSG)\n- Messi (PSG)\n- De Paul (Atl. Madrid)\n- Ángel Correa (Atl. Madrid)",
-            created_at: "2022-07-14T22:50:35+00:00",
-            retweets: 0,
-            likes: 0,
-            username: "Sonyzta",
-            name: "Lorenzo Di Pasquale 🇦🇷",
-            image: "https://pbs.twimg.com/profile_images/1532227145421860864/FuNuHl3A_normal.jpg",
-            sentiment: "positive"
-        } as TweetBody],
-        metrics: {}
-    } as TweetSentiments);
+        metrics: {},
+    } as TweetSentiments)
 
     return (
-        <div className={"md:h-screen h-full " + bgcolor}>
+        <div className={'h-full ' + bgcolor}>
             <Bar />
             <Search setTweets={setTweets} />
-            <div className="h-52" />
-            <Container className="h-max" fluid>
+            <div className='h-52' />
+            <Container className='h-max' fluid>
                 <Row>
                     <Metrics
                         positive={tweets.metrics.positives ?? 0.0}
@@ -45,29 +34,20 @@ export default function App() {
                         negative={tweets.metrics.negatives ?? 0.0}
                     />
                 </Row>
-                <div className="h-12" />
+                <div className='h-12' />
                 <Row xs={1} md={3}>
                     <Col>
-                        <Sentiment
-                            sentiment="negative"
-                            tweets={tweets.negatives ?? []}
-                        />
+                        <Sentiment sentiment='negative' tweets={tweets.negatives ?? []} />
                     </Col>
                     <Col>
-                        <Sentiment
-                            sentiment="neutral"
-                            tweets={tweets.neutral ?? []}
-                        />
+                        <Sentiment sentiment='neutral' tweets={tweets.neutral ?? []} />
                     </Col>
                     <Col>
-                        <Sentiment
-                            sentiment="positive"
-                            tweets={tweets.positives ?? []}
-                        />
+                        <Sentiment sentiment='positive' tweets={tweets.positives ?? []} />
                     </Col>
                 </Row>
             </Container>
-            <div className="h-12" />
+            <div className='h-12' />
         </div>
-    );
+    )
 }
--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,50 +1,66 @@
-import { useTheme } from "../stores/themes";
-import { TweetBody } from "../types/tweets";
-import { AiOutlineRetweet, AiOutlineHeart } from "react-icons/ai";
+import { motion } from 'framer-motion'
+import { useTheme } from '../stores/themes'
+import { TweetBody } from '../types/tweets'
+import { AiOutlineRetweet, AiOutlineHeart } from 'react-icons/ai'
 
 interface TweetProps {
-    body: TweetBody,
+    body: TweetBody
 }
 
 const Tweet = (props: TweetProps) => {
-    const url = "https://twitter.com/" + props.body.username + "/status/" + props.body.id;
+    const url =
+        'https://twitter.com/' + props.body.username + '/status/' + props.body.id
 
-    const { dark } = useTheme();
-    const bg = (dark) ? "bg-black" : "bg-white";
-    const tx = (dark) ? "text-white" : "test-dark";
+    const { dark } = useTheme()
+    const bg = dark ? 'bg-black' : 'bg-white'
+    const tx = dark ? 'text-white' : 'text-dark'
+
+    const date = new Date(props.body.created_at).toLocaleString()
+    const [days, hours] = date.split(',')
 
     return (
-        <>
-            <a href={url} className="no-underline" target="_blank" rel="noopener noreferrer">
-                <div className={"rounded-lg bg-black mt-2 " + bg}>
-                    <div className="grid grid-rows-1 grid-flow-col">
-                        <div className="row-span-1 col-span-1 flex justify-center mt-2">
-                            <img src={props.body.image} className="rounded-full" />
+        <motion.div animate={{ x: [0, 10, -10, 5, 0] }}>
+            <a
+                href={url}
+                className='no-underline'
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                <div className={'rounded-lg bg-black mt-2 ' + bg}>
+                    <div className='grid grid-cols-8'>
+                        <div className='col-span-1 mt-2 ml-2'>
+                            <img src={props.body.image} className='rounded-full' />
                         </div>
-                        <div className="row-span-1 mt-2">
-                            <p className={"mb-0 text-sm font-bold " + tx}>{props.body.name}</p>
-                            <p className="mb-0 text-gray-300 text-xs">@{props.body.username}</p>
+                        <div className='col-span-7 mt-2 ml-2'>
+                            <p className={'mb-0 text-sm font-bold ' + tx}>
+                                {props.body.name}
+                            </p>
+                            <p className='mb-0 text-gray-300 text-xs'>
+                                @{props.body.username}
+                            </p>
                         </div>
                     </div>
-                    <div className="mt-2">
-                        <p className={"text-sm ml-2 mb-0 " + tx}>{props.body.text}</p>
-                        <p className="text-sm text-gray-300 ml-2">{props.body.created_at}</p>
+                    <div className='mt-2'>
+                        <p className={'text-sm ml-2 mb-0 ' + tx}>{props.body.text}</p>
+                        <p className='text-sm text-gray-500 mt-2 ml-2'>
+                            {days} · {hours}
+                        </p>
                     </div>
-                    <hr className={"ml-2 mr-2 " + tx} />
-                    <div className="grid grid-rows-1 grid-cols-2">
-                        <div className="ml-6 flex">
-                            <AiOutlineRetweet className="text-zinc-500 mt-1" />
-                            <p className="text-zinc-500 ml-2">{props.body.retweets}</p>
+                    <hr className={'ml-2 mr-2 ' + tx} />
+                    <div className='grid grid-rows-1 grid-cols-2'>
+                        <div className='ml-6 flex'>
+                            <AiOutlineRetweet className='text-zinc-500 mt-1' />
+                            <p className='text-zinc-500 ml-2'>{props.body.retweets}</p>
                         </div>
-                        <div className="flex">
-                            <AiOutlineHeart className="text-zinc-500 mt-1" />
-                            <p className="text-zinc-500 ml-2">{props.body.likes}</p>
+                        <div className='flex'>
+                            <AiOutlineHeart className='text-zinc-500 mt-1' />
+                            <p className='text-zinc-500 ml-2'>{props.body.likes}</p>
                         </div>
                     </div>
                 </div>
             </a>
-        </>
+        </motion.div>
     )
-};
+}
 
-export default Tweet;
+export default Tweet
